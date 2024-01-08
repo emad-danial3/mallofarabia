98fa9058dc52dbe4071ce40bfdfbd4aa467a00d6
bishoy sobhy <bishoy.sobhy@ATR.local>
2024-01-08 10:56:49 +0200

    before emad update

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -29,10 +29,12 @@ class StoreController extends  HomeController
       return view('AdminPanel.PagesContent.store.closing_day_data', get_defined_vars());
         
     }
-    public function send_day_orders()
+    public function send_day_orders($day = false)
     {
-       $today = Carbon::today()->format('Y-m-d');
+
+       $today = $day == false ? Carbon::today()->format('Y-m-d') : $day ;
        $shifts = Shift::where('day',$today)->get();
+        
         $total_orders =  0 ;
         $all_lines = array();
         foreach( $shifts as $shift )
@@ -49,33 +51,44 @@ class StoreController extends  HomeController
 
               $product_id = $line->product_id;
               $quantity = $line->quantity;
-              $discount_rate = $line->discount_rate;
+              $discount_rate = (string) $line->discount_rate;
+              
               if(isset($all_lines[$product_id]))
               {
                   if(isset($all_lines[$product_id][$discount_rate]))
                   {
-                      $all_lines[$product_id][$discount_rate]+= $quantity ;
+                      $all_lines[$product_id][$discount_rate]['quantity']+= $quantity ;
                   }
                   else
                   {
-                      $all_lines[$product_id][$discount_rate] = $quantity ;
+                      $all_lines[$product_id][$discount_rate]['quantity'] = $quantity ;
+                      $all_lines[$product_id][$discount_rate]['price'] = $line->price ;
+                      $all_lines[$product_id][$discount_rate]['tax'] = $line->tax ;
                   }
               }
               else
               {
-                $all_lines[$product_id][$discount_rate] = $quantity ;
+                $all_lines[$product_id][$discount_rate]['quantity'] = $quantity ;
+                $all_lines[$product_id][$discount_rate]['price'] = $line->price ;
+                $all_lines[$product_id][$discount_rate]['tax'] = $line->tax ;
               }
             }
 
           }
         }
-         /*  $oracleInvoice = OracleCollectedInvoice::create(['total_amount' => $total_orders ]);
+           $oracleInvoice = OracleCollectedInvoice::create(['total_amount' => $total_orders ]);
 
       foreach( $shifts as $shift )
         {
           InvoiceShift::create(['shift_id' => $shift->id ,'invoice_collected_id' => $oracleInvoice->id ]);
-        }*/
-        var_dump($all_lines);
+        }
+
+        $client   = new \GuzzleHttp\Client();
+        $link = config('constants.save_order_link');
+        $response = $client->request('POST', $link, ['verify' => false, 'form_params' => array('items' => $all_lines,'id'=>'m-00'.$oracleInvoice->id)]);
+        $products = $response->getBody();
+        $products = json_decode($products, true);
+        dd($products);
          
     }
 }
--- config/constants.php
+++ config/constants.php
@@ -2,9 +2,8 @@
 
 
 return [
-    'save_order_link' => env('APP_ENV') === 'production' ? 'https://sales.atr-eg.com/api/save_order_mall_of_arabia_test.php':'https://sales.atr-eg.com/api/save_order_mall_of_arabia_test.php',
-    'refresh_order_link' => env('APP_ENV') === 'production' ? 'https://sales.atr-eg.com/api/RefreshNettinghubInvoices.php' :'https://sales.atr-eg.com/api/RefreshNettinghubInvoices.php',
-    'save_order_link' => env('APP_ENV') === 'production' ? 'https://sales.atr-eg.com/api/RefreshNettinghubInvoices.php' :'https://sales.atr-eg.com/api/RefreshNettinghubInvoices.php',
-    'item_avalability_link' => env('APP_ENV') === 'production' ? 'https://sales.atr-eg.com/api/get_NettinghubItems_item_avalability.php' :'https://sales.atr-eg.com/api/get_NettinghubItems_item_avalability.php',
+    'save_order_link' => env('APP_ENV') === 'production' ? 'https://sales.atr-eg.com/api/mallofarabiaTest.php':'http://sales.atr-eg.com/api/mallofarabiaTest.php',
+    'refresh_order_link' => env('APP_ENV') === 'production' ? 'https://sales.atr-eg.com/api/RefreshNettinghubInvoices.php' :'http://sales.atr-eg.com/api/RefreshNettinghubInvoices.php',
+    'item_avalability_link' => env('APP_ENV') === 'production' ? 'https://sales.atr-eg.com/api/get_NettinghubItems_item_avalability.php' :'http://sales.atr-eg.com/api/get_NettinghubItems_item_avalability.php',
   
 ]; 
