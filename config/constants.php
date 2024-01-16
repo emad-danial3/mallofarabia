ec222bb01b3c291029c169a2d1f80166b80d028f
bishoy sobhy <bishoy.sobhy@ATR.local>
2024-01-16 10:00:08 +0200

    limit invoice 50000 adjust link production and test

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 
 
 return [
-    'save_order_link' => env('APP_ENV') === 'production' ? 'https://sales.atr-eg.com/api/mallofarabiaTest.php':'http://sales.atr-eg.com/api/mallofarabiaTest.php',
+    'save_order_link' => env('APP_ENV') === 'production' ? 'https://sales.atr-eg.com/api/mallofarabia.php':'http://sales.atr-eg.com/api/mallofarabiaTest.php',
     'refresh_order_link' => env('APP_ENV') === 'production' ? 'https://sales.atr-eg.com/api/RefreshNettinghubInvoices.php' :'http://sales.atr-eg.com/api/RefreshNettinghubInvoices.php',
     'item_avalability_link' => env('APP_ENV') === 'production' ? 'https://sales.atr-eg.com/api/get_NettinghubItems_item_avalability.php' :'http://sales.atr-eg.com/api/get_NettinghubItems_item_avalability.php',
     'refresh_mall_items' => env('APP_ENV') === 'production' ? 'https://sales.atr-eg.com/api/RefreshMallItems.php' :'http://sales.atr-eg.com/api/RefreshMallItems.php',
